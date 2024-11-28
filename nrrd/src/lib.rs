15cafeaa5e780fa1658fa6a41bd5b1d0de2d2ea0
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2024-11-28 14:09:58 +0100

    Refactor, add tests

--- nrrd/src/lib.rs
+++ nrrd/src/lib.rs
@@ -6,6 +6,7 @@ use std::io::{BufRead, Read};
 use std::path::Path;
 use std::str::FromStr;
 
+
 type Metadata = std::collections::HashMap<String, String>;
 
 
@@ -89,6 +90,76 @@ pub enum NrrdType {
     BLOCK,
 }
 
+impl NrrdType {
+    fn from_string(string: &str) -> Result<NrrdType, String> {
+        match string {
+            "signed char"            |
+            "int8"                   |
+            "int8_t"                 => Ok(NrrdType::I8),
+            "unsigned char"          |
+            "uchar"                  |
+            "uint8"                  |
+            "uint8_t"                => Ok(NrrdType::U8),
+            "short"                  |
+            "short int"              |
+            "signed short"           |
+            "signed short int"       |
+            "int16"                  |
+            "int16_t"                => Ok(NrrdType::I16),
+            "ushort"                 |
+            "unsigned short"         |
+            "unsigned short int"     |
+            "uint16"                 |
+            "uint16_t"               => Ok(NrrdType::U16),
+            "int"                    |
+            "signed int"             |
+            "int32"                  |
+            "int32_t"                => Ok(NrrdType::I32),
+            "uint"                   |
+            "unsigned int"           |
+            "uint32"                 |
+            "uint32_t"               => Ok(NrrdType::U32),
+            "longlong"               |
+            "long long"              |
+            "long long int"          |
+            "signed long long"       |
+            "signed long long int"   |
+            "int64"                  |
+            "int64_t"                => Ok(NrrdType::I64),
+            "ulonglong"              |
+            "unsigned long long"     |
+            "unsigned long long int" |
+            "uint64"                 |
+            "uint64_t"               => Ok(NrrdType::U64),
+            "float"                  => Ok(NrrdType::F32),
+            "double"                 => Ok(NrrdType::F64),
+            "block"                  => Ok(NrrdType::BLOCK),
+            _ => Err(format!("Unknown data type : '{}'", string)),
+        }
+
+    }
+}
+
+
+pub enum NrrdEncoding {
+    RAW,
+    ASCII,
+    GZIP,
+    BZIP2,
+}
+
+impl NrrdEncoding {
+    fn from_string(string: &str) -> Result<NrrdEncoding, String> {
+        match string.to_lowercase().as_str() {
+            "raw" => Ok(NrrdEncoding::RAW),
+            "txt" | "text" | "ascii" => Ok(NrrdEncoding::ASCII),
+            "gz" | "gzip" => Ok(NrrdEncoding::GZIP),
+            "bz2" | "bzip2" => Ok(NrrdEncoding::BZIP2),
+            _ => Err(format!("Unsupported encoding {}", string)),
+        }
+    }
+}
+
 pub enum NrrdData {
     I8(Vec<i8>),
     U8(Vec<u8>),
@@ -119,53 +190,6 @@ impl NrrdData {
     }
 }
 
-fn lex_data_type(str_type: &str) -> Result<NrrdType, String> {
-    match str_type {
-        "signed char"            |
-        "int8"                   |
-        "int8_t"                 => Ok(NrrdType::I8),
-        "unsigned char"          |
-        "uchar"                  |
-        "uint8"                  |
-        "uint8_t"                => Ok(NrrdType::U8),
-        "short"                  |
-        "short int"              |
-        "signed short"           |
-        "signed short int"       |
-        "int16"                  |
-        "int16_t"                => Ok(NrrdType::I16),
-        "ushort"                 |
-        "unsigned short"         |
-        "unsigned short int"     |
-        "uint16"                 |
-        "uint16_t"               => Ok(NrrdType::U16),
-        "int"                    |
-        "signed int"             |
-        "int32"                  |
-        "int32_t"                => Ok(NrrdType::I32),
-        "uint"                   |
-        "unsigned int"           |
-        "uint32"                 |
-        "uint32_t"               => Ok(NrrdType::U32),
-        "longlong"               |
-        "long long"              |
-        "long long int"          |
-        "signed long long"       |
-        "signed long long int"   |
-        "int64"                  |
-        "int64_t"                => Ok(NrrdType::I64),
-        "ulonglong"              |
-        "unsigned long long"     |
-        "unsigned long long int" |
-        "uint64"                 |
-        "uint64_t"               => Ok(NrrdType::U64),
-        "float"                  => Ok(NrrdType::F32),
-        "double"                 => Ok(NrrdType::F64),
-        "block"                  => Ok(NrrdType::BLOCK),
-        _ => Err(format!("Unknown data type : '{}'", str_type)),
-    }
-
-}
 
 
 pub struct Nrrd {
@@ -174,9 +198,9 @@ pub struct Nrrd {
 }
 
 
-fn bytes_to_type<T: FromBuffer>(buffer: &[u8]) -> Vec<T>{
-   buffer
-       .chunks(std::mem::size_of::<T>())
+fn bytes_to_type<T: FromBuffer>(buffer: &[u8]) -> Vec<T> {
+    buffer
+       .chunks_exact(std::mem::size_of::<T>())
        .map(T::from_le_buffer)
        .collect()
 }
@@ -196,20 +220,19 @@ where <T as FromStr>::Err: Debug
 
 fn parse_array<T>(metadata: &Metadata, data: &[u8]) -> Vec<T>
 where
-    T: FromStr + FromBuffer,
+    T: FromStr + FromBuffer + Clone,
     <T as FromStr>::Err: Debug
 {
-
-    let sizes = parse_list::<usize>(metadata["sizes"].as_str());
-    let count: usize = sizes.iter().product();
-
-    let encoding = metadata["encoding"].as_str();
-    let type_size = std::mem::size_of::<T>();
+    let encoding = NrrdEncoding::from_string(metadata["encoding"].as_str()).unwrap();
 
     let numbers: Vec<T> = match encoding {
-        "ASCII" => text_to_type::<T>(&data),
-        "raw" => bytes_to_type::<T>(&data),
-        "bzip2" => {
+        NrrdEncoding::ASCII => text_to_type::<T>(&data),
+        NrrdEncoding::RAW => bytes_to_type::<T>(&data),
+        NrrdEncoding::BZIP2 => {
+            let sizes = parse_list::<usize>(metadata["sizes"].as_str());
+
+            let type_size = std::mem::size_of::<T>();
+            let count: usize = sizes.iter().product();
             let mut output = Vec::with_capacity(count * type_size);
             let mut decompressor = BzDecoder::new(data);
             match decompressor.read_to_end(&mut output) {
@@ -220,7 +243,10 @@ where
                 Err(e) => panic!("{e}"),
             }
         }
-        "gzip" => {
+        NrrdEncoding::GZIP => {
+            let sizes = parse_list::<usize>(metadata["sizes"].as_str());
+            let type_size = std::mem::size_of::<T>();
+            let count: usize = sizes.iter().product();
             let mut output = Vec::with_capacity(count * type_size);
             let mut decompressor = GzDecoder::new(data);
             match decompressor.read_to_end(&mut output) {
@@ -231,40 +257,91 @@ where
                 Err(e) => panic!("{e}"),
             }
         }
-        _ => panic!("Unknown encoding: '{}'", encoding),
     };
 
-    assert!(numbers.len() == count);
+    //assert!(numbers.len() == count);
     numbers
 
 }
 
 
-fn parse_data(metadata: &Metadata, data: &[u8]) -> NrrdData {
+pub fn read_data(metadata: &Metadata, buffer: &[u8]) -> NrrdData {
 
-    let data_type = lex_data_type(metadata["type"].as_str()).unwrap();
+    let data_type = NrrdType::from_string(metadata["type"].as_str()).unwrap();
 
     match data_type {
-        NrrdType::I8  => NrrdData::I8(parse_array::<i8>(&metadata, &data)),
-        NrrdType::U8  => NrrdData::U8(parse_array::<u8>(&metadata, &data)),
-        NrrdType::I16 => NrrdData::I16(parse_array::<i16>(&metadata, &data)),
-        NrrdType::U16 => NrrdData::U16(parse_array::<u16>(&metadata, &data)),
-        NrrdType::I32 => NrrdData::I32(parse_array::<i32>(&metadata, &data)),
-        NrrdType::U32 => NrrdData::U32(parse_array::<u32>(&metadata, &data)),
-        NrrdType::I64 => NrrdData::I64(parse_array::<i64>(&metadata, &data)),
-        NrrdType::U64 => NrrdData::U64(parse_array::<u64>(&metadata, &data)),
-        NrrdType::F32 => NrrdData::F32(parse_array::<f32>(&metadata, &data)),
-        NrrdType::F64 => NrrdData::F64(parse_array::<f64>(&metadata, &data)),
+        NrrdType::I8  =>   NrrdData::I8(parse_array::<i8>(&metadata, &buffer)),
+        NrrdType::U8  =>   NrrdData::U8(parse_array::<u8>(&metadata, &buffer)),
+        NrrdType::I16 => NrrdData::I16(parse_array::<i16>(&metadata, &buffer)),
+        NrrdType::U16 => NrrdData::U16(parse_array::<u16>(&metadata, &buffer)),
+        NrrdType::I32 => NrrdData::I32(parse_array::<i32>(&metadata, &buffer)),
+        NrrdType::U32 => NrrdData::U32(parse_array::<u32>(&metadata, &buffer)),
+        NrrdType::I64 => NrrdData::I64(parse_array::<i64>(&metadata, &buffer)),
+        NrrdType::U64 => NrrdData::U64(parse_array::<u64>(&metadata, &buffer)),
+        NrrdType::F32 => NrrdData::F32(parse_array::<f32>(&metadata, &buffer)),
+        NrrdType::F64 => NrrdData::F64(parse_array::<f64>(&metadata, &buffer)),
         _ => panic!("Unkown data type encoding.")
     }
 }
 
-fn parse_list<T: std::str::FromStr>(data: &str) -> Vec<T>
+pub fn parse_list<T: std::str::FromStr>(data: &str) -> Vec<T>
 where
     <T as FromStr>::Err: Debug,
 {
     data.split(' ').map(|v| v.parse::<T>().unwrap()).collect()
 }
+
+
+pub fn read_metadata(buffer: &[u8]) -> (Metadata, usize) {
+
+    let mut offset: usize = 0;
+    let mut lines = buffer.lines();
+    if let Some(Ok(line)) = lines.next() {
+        if line != "NRRD0003" && line != "NRRD0004" && line != "NRRD0005" {
+            panic!("Incorrect magic line: '{}'", line);
+        }
+        offset += line.len() + 1;
+    } else {
+        panic!();
+    }
+
+    let mut metadata = Metadata::new();
+    while let Some(Ok(line)) = lines.next() {
+        offset += line.len() + 1;
+        if line.is_empty() {
+            break;
+        }
+        if let Some((k, v)) = line.split_once(':') {
+            let k = k.trim().to_string();
+            let mut v = v.trim().to_string();
+
+            if k.starts_with('#') {
+                continue;
+            } else if v.starts_with('=') {
+                v = v[1..].trim().to_string();
+            }
+            metadata.insert(k, v);
+        }
+    }
+    if !metadata.contains_key("sizes") {
+        panic!("Missing `sizes` in header")
+    } else if !metadata.contains_key("encoding") {
+        panic!("Missing `encoding` in header")
+    } else if !metadata.contains_key("dimension") {
+        panic!("Missing `dimension` in header")
+    }
+
+    (metadata, offset)
+}
+
+
+pub fn read_file(path: &Path) -> (Metadata, NrrdData) {
+    let buffer = fs::read(path).unwrap();
+    let (metadata, offset) = read_metadata(&buffer);
+    let data = read_data(&metadata, &buffer[offset..]);
+    (metadata, data)
+}
+
 
 impl Nrrd {
     pub fn sizes(self: &Self) -> Vec<usize> {
@@ -272,48 +349,8 @@ impl Nrrd {
     }
 
     pub fn from_buffer(buf: &[u8]) -> Self {
-        let mut offset: usize = 0;
-        let mut lines = buf.lines();
-        if let Some(Ok(line)) = lines.next() {
-            if line != "NRRD0003" && line != "NRRD0004" && line != "NRRD0005" {
-                panic!("Incorrect magic line: '{}'", line);
-            }
-            offset += line.len() + 1;
-        } else {
-            panic!();
-        }
-
-        let mut metadata = Metadata::new();
-        while let Some(Ok(line)) = lines.next() {
-            offset += line.len() + 1;
-            if line.is_empty() {
-                break;
-            }
-            if let Some((k, v)) = line.split_once(':') {
-                let k = k.trim().to_string();
-                let mut v = v.trim().to_string();
-
-                if k.starts_with('#') {
-                    continue;
-                } else if v.starts_with('=') {
-                    v = v[1..].trim().to_string();
-                }
-                metadata.insert(k, v);
-            }
-        }
-        if !metadata.contains_key("sizes") {
-            panic!("Missing `sizes` in header")
-        } else if !metadata.contains_key("encoding") {
-            panic!("Missing `encoding` in header")
-        } else if !metadata.contains_key("dimension") {
-            panic!("Missing `dimension` in header")
-        }
-
-        let data = parse_data(
-            &metadata,
-            &buf[offset..],
-        );
-
+        let (metadata, offset) = read_metadata(&buf);
+        let data = read_data(&metadata, &buf[offset..]);
         Self { metadata, data }
     }
 
@@ -326,6 +363,41 @@ impl Nrrd {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn vec_compare<T: std::cmp::PartialEq>(va: &[T], vb: &[T]) -> bool {
+        (va.len() == vb.len()) &&  // zip stops at the shortest
+         va.iter()
+           .zip(vb)
+           .all(|(a,b)| { *a == *b} )
+    }
+
+    macro_rules! test_read_data_le {
+        ($data_type:ty, $variant:path, $type_aliases:expr, $numbers:expr) => {
+            {
+                let bytes: Vec<u8> = $numbers.iter().flat_map(|&num| num.to_le_bytes()).collect();
+
+                for type_synonym in $type_aliases {
+                    let metadata = Metadata::from([
+                        ("type".to_string(), type_synonym.to_string()),
+                        ("dimension".to_string(), "1".to_string()),
+                        ("sizes".to_string(), $numbers.len().to_string()),
+                        ("encoding".to_string(), "raw".to_string()),
+                        ("endian".to_string(), "little".to_string()),
+                    ]);
+
+                    let data = read_data(&metadata, &bytes);
+
+                    if let $variant(values) = data {
+                        assert_eq!(vec_compare::<$data_type>(&$numbers, &values), true);
+                    } else {
+                        panic!("Wrong Variant.");
+                    }
+                }
+            }
+        };
+    }
+
+
 
     #[test]
     fn parse_header() {
@@ -400,5 +472,50 @@ mod tests {
         } else {
             panic!("Wrong variant");
         }
+    }
+
+    #[test]
+    fn test_read_data_le() {
+
+        let numbers: [i8; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["signed char", "int8", "int8_t"];
+        test_read_data_le!(i8, NrrdData::I8, aliases, numbers);
+
+        let numbers: [u8; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["unsigned char", "uchar", "uint8", "uint8_t"];
+        test_read_data_le!(u8, NrrdData::U8, aliases, numbers);
+
+        let numbers: [i16; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["short", "short int", "signed short", "signed short int", "int16", "int16_t"];
+        test_read_data_le!(i16, NrrdData::I16, aliases, numbers);
+
+        let numbers: [u16; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["ushort", "unsigned short", "unsigned short int", "uint16", "uint16_t"];
+        test_read_data_le!(u16, NrrdData::U16, aliases, numbers);
+
+        let numbers: [i32; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["int", "signed int", "int32", "int32_t"];
+        test_read_data_le!(i32, NrrdData::I32, aliases, numbers);
+
+        let numbers: [u32; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["uint", "unsigned int", "uint32", "uint32_t"];
+        test_read_data_le!(u32, NrrdData::U32, aliases, numbers);
+
+        let numbers: [i64; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["longlong", "long long", "signed long long", "signed long long int", "int64", "int64_t"];
+        test_read_data_le!(i64, NrrdData::I64, aliases, numbers);
+
+        let numbers: [u64; 5] = [5, 10, 15, 20, 25];
+        let aliases = ["ulonglong", "unsigned long long", "unsigned long long int", "uint64", "uint64_t"];
+        test_read_data_le!(u64, NrrdData::U64, aliases, numbers);
+
+        let numbers: [f32; 5] = [5.0, 10.0, 15.0, 20.0, 25.0];
+        let aliases = ["float"];
+        test_read_data_le!(f32, NrrdData::F32, aliases, numbers);
+
+        let numbers: [f64; 5] = [5.0, 10.0, 15.0, 20.0, 25.0];
+        let aliases = ["double"];
+        test_read_data_le!(f64, NrrdData::F64, aliases, numbers);
+
     }
 }
